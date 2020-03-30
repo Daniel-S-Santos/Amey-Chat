09edd0d184d0f6a5b54bc372ebfd1ba07ce4a90c
Sistemas GrupoABL <sistemas@gabl.lan>
2020-03-30 08:19:55 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+//require('./GxPost')
+
+const port = process.env.PORT || 3000
+
+io.on('connection', function (socket) {
+    console.log('connection.....')
+    
+
+    var salas =[]
+
+    socket.on('disconnect', function(){
+        console.log('desconectado')
+    });
+    
+    socket.on('create', function (room,name) { 
+        socket.leaveAll()       
+        socket.join(room)  
+            
+        console.log(`${name}`,'entrou na',room)
+        //socket.broadcast.emit('Entrou',`${socket.id} Entrou na sala ${room}`)        
+    });  
+    socket.on('mensagem', function (room,name,msg) {
+        //console.log(`${nome}`,'mandou:', data); 
+        //socket.to(room).emit('Fulano',`${room,nome,data}`)
+        
+        //console.log(room+ ' nome '+name+ ' '+msg)        
+        io.in(room).emit('msgg',room ,name ,msg)        
+        //io.emit('mensagem',room ,name ,msg )
+
+        
+        console.log('disse ',msg)
+        //socket.emit('Me',data)
+    }); 
+
+});
+
+
+server.listen(port, () => {
+    console.log('server rodando na porta '+port)
+});
+
+
+
+
+//socket.on('chat message', (msg)=>{$('#messages').append($('<li>').text(msg));});
